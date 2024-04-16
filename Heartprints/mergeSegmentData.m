d12ea77f243baec8244af948bca42708a0b087d1
hnisofia <158060419+hnisofia@users.noreply.github.com>
2024-04-16 04:20:00 +0100

    Removed tm field

--- Heartprints/mergeSegmentData.m
+++ Heartprints/mergeSegmentData.m
@@ -1,5 +1,5 @@
 function combinedData = mergeSegmentData(sinus_intervals,ventricular_intervals,NIB,CI)
-    combinedData=struct('sinus_intervals', [], 'ventricular_intervals', [], 'CI', [], 'NIB', [], 'tm', []);
+    combinedData=struct('sinus_intervals', [], 'ventricular_intervals', [], 'CI', [], 'NIB', []);
     
     combinedData.sinus_intervals=[combinedData.sinus_intervals;sinus_intervals];
     combinedData.ventricular_intervals=[combinedData.ventricular_intervals;ventricular_intervals];
