29d94b6f290876567aa68f8ef9c5a6125cd210f5
hnisofia <158060419+hnisofia@users.noreply.github.com>
2024-04-16 04:22:09 +0100

    Update calculateCI.m

--- Heartprints/calculateCI.m
+++ Heartprints/calculateCI.m
@@ -1,3 +1,4 @@
 function CI=calculateCI(sinus_time, ectopic_times)
-    CI=arrayfun(@(et) et -sinus_time(find(sinus_time< et, 1, 'last')), ectopic_times);
+    CI=arrayfun(@(et) et-sinus_time(find(sinus_time< et, 1, 'last')), ectopic_times,'UniformOutput',false);
+    CI=cell2mat(CI(~cellfun('isempty', CI)));
 end 
